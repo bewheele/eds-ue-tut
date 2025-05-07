e835e797fc89e85274c9395783e0bc95430ed0c6
Benjie Wheeler <bewheele@adobe.com>
2025-05-07 09:48:38 -0600

    teaser json added

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -4,5 +4,4 @@ export default function decorate(block) {
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
-
 }
